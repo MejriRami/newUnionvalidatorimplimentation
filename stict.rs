d86ad90a453876de18705bdc1648d2e56c3871fa
MejriRami <134556203+MejriRami@users.noreply.github.com>
2025-05-20 14:29:26 +0100

    Update stict.rs

--- stict.rs
+++ stict.rs
@@ -1,2 +1,15 @@
+impl CombinedValidator {
+    pub fn strict(&self) -> Option<bool> {
+        match self {
+            CombinedValidator::StrictString(v) => Some(v.strict),
+            CombinedValidator::StrictInt(v) => Some(v.strict),
+            CombinedValidator::StrictFloat(v) => Some(v.strict),
+            CombinedValidator::StrictBool(v) => Some(v.strict),
+            CombinedValidator::List(v) => v.strict,
+            CombinedValidator::Dict(v) => v.strict,
+            // Add any other variants that support strictness here...
 
-
+            _ => None,
+        }
+    }
+}
